050a47732aa0481bb8d52a5fa48d284c87991442
TonyDev92 <tonydeveloper92@gmail.com>
2023-07-14 10:44:20 +0200

    AuthUpdates

--- src/api/routes/product.routes.js
+++ src/api/routes/product.routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const upload = require('../../middleware/upload');
+const { isAuth } = require('../../middleware/auth');
 const router = express.Router();
 
 const { getProduct, getProductById , postProduct , putProduct , deleteProduct } = require('../controllers/product.controller');
 
 router.get('/' , getProduct);
 router.get('/:id', getProductById);
-router.post('/' , upload.single('image'), postProduct);
-router.put('/:id',upload.single('image'),  putProduct);
-router.delete('/:id', deleteProduct);
+router.post('/' , [isAuth], upload.single('image'), postProduct);
+router.put('/:id', [isAuth], upload.single('image'),  putProduct);
+router.delete('/:id',[isAuth] , deleteProduct);
 
 module.exports = router;
